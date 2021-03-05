25bbd3a7c97239b7eedba6e487a871358663a805
dpride07 <69624086+dpride07@users.noreply.github.com>
2021-03-05 15:27:28 +0800

    server update - -new with back end for login and product function

--- ifind-server/server.js
+++ ifind-server/server.js
@@ -1,38 +1,42 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/auth');
+
+const postsRoutes = require('./routes/posts');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+const ports = process.env.PORT || 3000;
 
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const db = require("./app/models");
-
-db.sequelize.sync();
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Ifindilu" });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Accept, X-Custom-Header, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
 });
 
+app.use('/auth', authRoutes);
 
+app.use('/post', postsRoutes);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+app.use(errorController.get404);
+
+app.use(errorController.get500);
+
+app.listen(ports, () => console.log(`Listening on port ${ports}`));
